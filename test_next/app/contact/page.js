7b74d71dd98f7709d1777314a01e1cb4312a395a
Carlos O <orjuelacarlos80@gmail.com>
2024-06-20 11:22:29 -0500

    rutas estaticas y dinamicas en next js

---
+++ test_next/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact(){
+    return(
+        <div>
+            <h1>
+                Hola, esto es contacto
+            </h1>
+        </div>
+    )
+}
---
+++ test_next/app/platforms/[platform]/page.js
@@ -0,0 +1,9 @@
+export default function Platform( { params }){
+    return(
+        <div>
+            <h1>
+                plataforma {params.platform}
+            </h1>
+        </div>
+    )
+}
